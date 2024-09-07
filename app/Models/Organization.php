243b5746a88f3389305ad129f46009b0c9703538
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-09-07 18:45:38 +0900

    テーブル構造を変更

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -11,6 +11,15 @@ class Organization extends Model
 
     protected $table = 'organizations';
 
+    // IDのカラム名
+    protected $primaryKey = 'id';
+
+    // 自動インクリメントを使用しない
+    public $incrementing = false;
+
+    // IDの型
+    protected $keyType = 'string';
+
     public function employees()
     {
         return $this->belongsToMany(Employee::class, 'organization_employee', 'organization_id', 'employee_id');
