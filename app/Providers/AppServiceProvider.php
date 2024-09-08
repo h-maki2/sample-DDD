f64147e082752aff85c231804b9a7ae5423e4e9c
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-09-08 23:36:47 +0900

    サービスプロバイダーを登録

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,15 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use organizationManagement\sqlInfrastructure\persistence\EloquentEmployeeRepository;
+use organizationManagement\sqlInfrastructure\persistence\EloquentOrganizationQueryService;
+use organizationManagement\sqlInfrastructure\persistence\EloquentOrganizationRepository;
+use organizationManagement\sqlInfrastructure\persistence\EloquentUnitOfWork;
+use organizationManagement\application\organization\IOrganizationQueryService;
+use organizationManagement\domain\model\employee\IEmployeeRepository;
+use organizationManagement\domain\model\organization\IOrganizationRepository;
+use organizationManagement\domain\model\common\AUnitOfWork;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -11,7 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(IOrganizationRepository::class, EloquentOrganizationRepository::class);
+        $this->app->bind(IOrganizationQueryService::class, EloquentOrganizationQueryService::class);
+        $this->app->bind(AUnitOfWork::class, EloquentUnitOfWork::class);
+        $this->app->bind(IEmployeeRepository::class, EloquentEmployeeRepository::class);
     }
 
     /**
