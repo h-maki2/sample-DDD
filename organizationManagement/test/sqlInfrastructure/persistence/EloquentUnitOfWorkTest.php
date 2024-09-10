42d072b5de06b65956d985292c4c16e482d2dfee
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2024-09-10 23:25:25 +0900

    EloquentUnitOfWorkTestクラスを作成

--- organizationManagement/test/sqlInfrastructure/persistence/EloquentUnitOfWorkTest.php
+++ organizationManagement/test/sqlInfrastructure/persistence/EloquentUnitOfWorkTest.php
@@ -35,4 +35,25 @@ class EloquentUnitOfWorkTest extends TestCase
         $this->assertEquals($employee->name()->value(), $reconstructedEmployee->name()->value());
         $this->assertEquals($employee->isRetired(), $reconstructedEmployee->isRetired());
     }
+
+    public function test_トランザクションが正常にロールバックできる()
+    {
+        // given: テストデータの準備
+        $testEmployeeFactory = new TestEmployeeFactory();
+        $employeeId = $this->employeeRepository->nextIdentity();
+        $employee = $testEmployeeFactory->create($employeeId);
+
+        // when: トランザクションの中で例外を発生させる
+        try {
+            $this->unitOfWork->performTransaction(function () use ($employee) {
+                $this->employeeRepository->save($employee);
+                throw new Exception();
+            });
+        } catch (Exception $e) {   
+        }
+        $reconstructedEmployee = $this->employeeRepository->findById($employeeId);
+
+        // then: 結果が取得できないことを確認
+        $this->assertNull($reconstructedEmployee);
+    }
 }
